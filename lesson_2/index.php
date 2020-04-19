a4468cc77fa1f890538ce425179ff32e479351f2
Holyskretch <alex03091993@gmail.com>
2020-04-19 22:09:38 +0300

    2

---
+++ lesson_2/index.php
@@ -0,0 +1,114 @@
+<?php 
+/*
+//задание 1
+$a = rand(-5,5);
+$b = rand(-5,5);
+
+if ($a >= 0 && $b >= 0){
+    
+    echo $a - $b;    
+}
+elseif ($a < 0 && $b < 0){
+    echo $a * $b;
+}
+elseif(($a < 0 && $b >= 0)||($a >= 0 && $b < 0)){
+    echo $a + $b;
+}
+*/
+
+/*
+//задание 2
+$a = rand(0,15);
+    switch ($a){
+        case 0:
+            echo ($a++." ");
+        case 1:
+            echo ($a++." ");
+        case 2:
+            echo ($a++." ");
+        case 3:
+            echo ($a++." ");
+        case 4:
+            echo ($a++." ");
+        case 5:
+            echo ($a++." ");
+        case 6:
+            echo ($a++." ");
+        case 7:
+            echo ($a++." ");
+        case 8:
+            echo ($a++." ");
+        case 9:
+            echo ($a++." ");
+        case 10:
+            echo ($a++." ");
+        case 11:
+            echo ($a++." ");
+        case 12:
+            echo ($a++." ");
+        case 13:
+            echo ($a++." ");
+        case 14:
+            echo ($a++." ");
+        case 15:
+            echo ($a++." ");
+        case 16:
+            echo ($a++." ");
+        case 17:
+            echo ($a++." ");
+        case 18:
+            echo ($a++." ");
+        case 19:
+            echo ($a++." ");
+        case 20:
+            echo ($a." ");
+        break; 
+}
+*/
+
+/*
+//задание 3
+function sum($a,$b){
+    return $a + $b;
+}
+
+function sub($a,$b){
+    return $a - $b;
+}
+
+function mult($a,$b){
+    return $a * $b;
+}
+
+function div($a,$b){
+    return $a / $b;
+}
+
+// echo sum(6,2)." ";
+// echo sub(6,2)." ";
+// echo mult(6,2)." ";
+// echo div(6,2)." ";
+
+
+// Задание 4
+
+function mathOperation($arg1, $arg2, $operation){
+    switch ($operation){
+        case "+":
+            echo sum($arg1,$arg2);
+            break;
+        case "-":
+            echo sub($arg1,$arg2);
+            break;
+        case "*":
+            echo mult($arg1,$arg2);
+            break;
+        case "/":
+            echo div($arg1,$arg2);
+            break;
+    }
+}
+
+//mathOperation(6, 2, "/");
+*/
+
