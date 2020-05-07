4ebbfd18d12d495a187a4616f920f3536da1af24
Holyskretch <alex03091993@gmail.com>
2020-05-07 17:01:54 +0300

    lesson5

---
+++ lesson_5/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>    
+        <h1>Галерея фотографий</h1>
+        <div class="gallery">
+            <?php
+            include 'showimg.php';
+            ?>
+        </div> 
+</body>
+</html>
